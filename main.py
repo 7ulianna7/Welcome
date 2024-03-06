3c26e7fdf3ba391029ac46e150523cbf6f66e62a
Julia <balabanka123456@gmail.com>
2024-03-06 12:07:43 +0300

    Небольшой фикс.2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Edge(pg.sprite.Sprite):
     '''
         Класс ребер граба
     '''
-    def __init__(self, name, screen, color=GRAY, size = 3, pos = None):
+    def __init__(self, name, screen, color=GRAY, size=3, pos1=None, pos2=None):
         self.name = name
         self.color = color
         self.screen = screen
@@ -19,7 +19,7 @@ class Edge(pg.sprite.Sprite):
         self.image = pg.Surface((size, size))
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
-        self.pos = pos
+        self.pos = (pos1, pos2)
     def update(self):
         pg.draw.line(self.screen, self.color, self.pos[0], self.pos[1], self.size)
         self.screen.blit(self.image, (self.pos[0][0], self.pos[0][1]))
@@ -38,17 +38,15 @@ class VertexClass(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         if pos:
             self.rect.x, self.rect.y = pos
-            #print(self.rect.x, self.rect.y)
+            self.center = (self.rect.x + 20, self.rect.y + 20)
 
     def update(self):
         pg.draw.circle(self.image, self.color, (self.size // 2,self.size // 2), self.size // 2)
-        #self.screen.blit(self.image, (self.rect.x-20, self.rect.y-20))
         self.screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
 i = 0
 j = 0
-a = []
 b = []
 graph = []
 class VertexGroup(pg.sprite.Group):
@@ -76,11 +74,10 @@ class VertexGroup(pg.sprite.Group):
                         i += 1
         super().update()
     def addedge(self, v, pos, screen):
-        global a, b, j, graph
+        global b, j, graph
         b.append(v)
-        a.append(pos)
-        if len(a) == 2:
-            A = Edge(j, screen, pos = a)
+        if len(b) == 2:
+            A = Edge(j, screen, pos1 = b[0].center, pos2 = b[1].center)
             A.update()
             j += 1
             b[0].color = GRAY
@@ -89,7 +86,6 @@ class VertexGroup(pg.sprite.Group):
             graph.append((b[1].name, b[0].name))
             b[0].update()
             b[1].update()
-            a.clear()
             b.clear()
 
 
