5767c57aed703b747fee4d6887e91e0bcda2ebde
Julia <balabanka123456@gmail.com>
2024-03-07 11:43:44 +0300

    Документирование

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ num = 20
 
 class Edge(pg.sprite.Sprite):
     '''
-        Класс ребер граба
+        Класс ребер графа
     '''
     def __init__(self, v1, v2, screen, color=GRAY, size=3, pos1=None, pos2=None):
         super().__init__()
@@ -28,6 +28,7 @@ class Edge(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.pos = (pos1, pos2)
     def update(self):
+        # обновление картинки ребер
         global text, screen
         pg.draw.line(self.screen, self.color, self.pos[0], self.pos[1], self.size)
         self.screen.blit(self.image, (self.pos[0][0], self.pos[0][1]))
@@ -37,6 +38,7 @@ class EdgeGroup(pg.sprite.Group):
         Группа ребер графа
     '''
     def update(self, a):
+        # добавляем ребро в группу и обновляем
         self.add(a)
         super().update()
 
@@ -59,6 +61,7 @@ class VertexClass(pg.sprite.Sprite):
             self.center = (self.rect.x + 20, self.rect.y + 20)
 
     def update(self):
+        # обновляем картинку вершин
         pg.draw.circle(self.image, self.color, (self.size // 2,self.size // 2), self.size // 2)
         self.screen.blit(self.image, (self.rect.x, self.rect.y))
 
@@ -80,11 +83,13 @@ class VertexGroup(pg.sprite.Group):
                 # проверяем каждую из вершин в группе, был ли по ней клик
                 for v in self.spritedict:
                     if v.rect.collidepoint(event.pos):
+                        # если был клик, то считаем одной из вершин будущего ребра, меняем цвет
                         self.ch = True
                         if v.color == GRAY:
                             v.color = PINK
                             self.addedge(v, pg.mouse.get_pos(), screen)
                 if self.ch is False:
+                    # если не было клика по вершине, значит создаем в этой точке новую вершину
                     if not St.buttonRect.collidepoint(pg.mouse.get_pos()):
                         self.add(VertexClass(i, screen, pos=pg.mouse.get_pos()))
                         i += 1
@@ -93,6 +98,7 @@ class VertexGroup(pg.sprite.Group):
         global b, graph, ed
         b.append(v)
         if len(b) == 2:
+            #если было кликнуто по двум вершинам, создаем ребро
             A = Edge(b[0].name, b[1].name, screen, pos1 = b[0].center, pos2 = b[1].center)
             ed.update(A)
             b[0].color = GRAY
@@ -104,6 +110,7 @@ class VertexGroup(pg.sprite.Group):
 
 
 def myFunction():
+    # создаем граф, вызываем функцию Форда-Беллмана
     global b, gr
     G = Graph(graph, i)
     if len(b) == 0:
@@ -118,6 +125,7 @@ def myFunction():
         print("Расстояние от вершины", b[0].name, "до", j, "->", ans[j])
 
 def upd(v):
+    # меняем цвет вершины/ребра, если наша программа в неё заходит/из неё выходит
     if v.color == PINK:
         v.color = GRAY
         v.update()
@@ -129,6 +137,7 @@ def upd(v):
     pygame.time.delay(500)
 
 def bellman_ford(graph, n, s):
+    # ункция обхода графа Беллмана-Форда
     global gr, screen, ed
     dist = [float('inf')] * n
     dist[s] = 0
@@ -136,25 +145,31 @@ def bellman_ford(graph, n, s):
         for src, dest, weight in graph:
             for v in gr.spritedict:
                 if v.name == src:
+                    # меняем цвет вершины, в которой сейчас находимся
                     upd(v)
                     for k in ed.spritedict:
                         if k.v1 == src and k.v2 == dest:
+                            # меняем цвет ребра, в котором сейчас находимся
                             upd(k)
                             break
                     break
             for l in gr.spritedict:
                 if l.name == dest:
+                    # меняем цвет вершины, в которую собираемся пойти
                     upd(l)
                     break
 
             if dist[src] + weight < dist[dest]:
                 dist[dest] = dist[src] + weight
+
             print("Расстояние от начальной вершины до "+str(dest)+":", dist[dest])
 
         for e in gr.spritedict:
+            # после одного цикла возвращаем всем вершинам серый цвет
             e.color = GRAY
             e.update()
         for l in ed.spritedict:
+            # после одного цикла возвращаем всем ребрам серый цвет
             l.color = GRAY
             l.update()
         pygame.display.update()
@@ -168,8 +183,11 @@ screen.fill(WHITE)
 
 # создаём вершины и помещаем их в группу
 gr = VertexGroup()
+# создаём ребра и помещаем их в группу
 ed = EdgeGroup()
+# создаём кнопу для старта обхода графа
 St = Button(screen, 450, 540, 300, 50, 'Нажми, чтобы начать обход по графу', myFunction)
+
 running = True
 while running:
     events = pg.event.get()
@@ -178,7 +196,7 @@ while running:
             running = False
     for object in objects:
         object.process(screen)
-    # список полученных событий передаётся как параметр в функцию update группы вершин
+    # список полученных событий передаётся как параметр в функцию update группы вершин и ребер
     gr.update(events, screen)
     pg.display.update()
     pg.time.delay(100)
